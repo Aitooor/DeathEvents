9be3ba9b388c71b0ced515dde1f3cad0f8b44ba2
iTito420 <aitorarias19@gmail.com>
2022-12-09 02:30:19 +0100

    fix: change listeners to separated classes

--- src/main/java/com/github/aitooor/deathevents/liteners/DeathListener.java
+++ src/main/java/com/github/aitooor/deathevents/liteners/DeathListener.java
@@ -1,33 +1,25 @@
-package com.github.aitooor.deathevents;
+package com.github.aitooor.deathevents.liteners;
 
+import com.github.aitooor.deathevents.DeathEvents;
 import com.github.aitooor.deathevents.config.ConfigFile;
+import com.github.aitooor.deathevents.config.MessageFile;
+import com.github.aitooor.deathevents.utils.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class DeathListener implements Listener {
     private DeathEvents plugin;
     private ConfigFile config;
+    private MessageFile messageFile;
 
-    public DeathListener(DeathEvents plugin, ConfigFile config) {
+    public DeathListener(DeathEvents plugin, ConfigFile config, MessageFile messageFile) {
         this.plugin = plugin;
         this.config = config;
-    }
-
-    @EventHandler
-    public void onPlayerRespawn(PlayerRespawnEvent e) {
-        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-
-        Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            for(String commandList : config.getStringList("respawn_events.commands")) {
-                commandList = commandList.replace("%player%", e.getPlayer().getName());
-                Bukkit.dispatchCommand(console, commandList);
-            }
-        }, 4L);
+        this.messageFile = messageFile;
     }
 
     @EventHandler
@@ -36,6 +28,13 @@ public class DeathListener implements Listener {
         Player player = e.getEntity().getPlayer();
 
         if(!(player instanceof Player)) return;
+
+        if(!config.getBoolean("death_events.enable")) {
+            if(player.hasPermission("deathevents.admin")) {
+                ChatUtils.send(player, messageFile.getString("death_events.no_enabled"));
+            }
+            return;
+        }
 
         Bukkit.getScheduler().runTaskLater(plugin, () -> {
             for(String commandList : config.getStringList("death_events.commands")) {
---
+++ src/main/java/com/github/aitooor/deathevents/liteners/RespawnListener.java
@@ -0,0 +1,47 @@
+package com.github.aitooor.deathevents.liteners;
+
+import com.github.aitooor.deathevents.DeathEvents;
+import com.github.aitooor.deathevents.config.ConfigFile;
+import com.github.aitooor.deathevents.config.MessageFile;
+import com.github.aitooor.deathevents.utils.ChatUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+public class RespawnListener implements Listener {
+    private DeathEvents plugin;
+    private ConfigFile config;
+    private MessageFile messageFile;
+
+    public RespawnListener(DeathEvents plugin, ConfigFile config, MessageFile messageFile) {
+        this.plugin = plugin;
+        this.config = config;
+        this.messageFile = messageFile;
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent e) {
+        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+        Player player = e.getPlayer();
+
+        if(!(player instanceof Player)) return;
+
+        if(!config.getBoolean("respawn_events.enable")) {
+            if(player.hasPermission("deathevents.admin")) {
+                ChatUtils.send(player, messageFile.getString("respawn_events.no_enabled"));
+            }
+            return;
+        }
+
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            for(String commandList : config.getStringList("respawn_events.commands")) {
+                commandList = commandList.replace("%player%", e.getPlayer().getName());
+                Bukkit.dispatchCommand(console, commandList);
+            }
+        }, 4L);
+    }
+}
